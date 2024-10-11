7f406767b1401c53193ac392f84d72052fc2dcf7
Damian Figiel <damian.figiel@enjin.io>
2024-10-11 14:49:35 +0200

    Added JWT authentication middleware

---
+++ auth_middleware.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtSecret = []byte(os.Getenv("GITHUB_OWNER"))
+
+func JWTAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
